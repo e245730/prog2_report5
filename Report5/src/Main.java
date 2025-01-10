b2868fbb9b34bd050191bd638f8fc016975ae56b
toguchisouho@toguchikanadefunonotobukkukonpyuta <e245730@ie.u-ryukyu.ac.jp>
2025-01-10 12:34:05 +0900

    report5_step3

--- Report5/src/Main.java
+++ Report5/src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-       try {
-         String str = null;
-         System.err.println(str.length());
-       } catch (NullPointerException e){
-            System.err.println("NullPointerExceptiが発生しました。");
-
-            System.err.println("エラーメッセージ:"+e.getMessage());
-       }
-
-    }
-
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.err.println(value);
     
+    }
 }
