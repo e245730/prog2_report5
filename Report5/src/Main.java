d007712d6756fc6264c6d88f9306a1afa01ca712
toguchisouho@toguchikanadefunonotobukkukonpyuta <e245730@ie.u-ryukyu.ac.jp>
2025-01-10 11:44:02 +0900

    first commit

---
+++ Report5/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String str = null;
+        System.err.println(str.length());
+
+    }
+
+    
+}
